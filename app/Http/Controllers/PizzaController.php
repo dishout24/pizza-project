6ab8ed9092d49e0847d75a33b3b163ee2bf57478
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2023-10-30 10:54:00 +0200

    let's begin

---
+++ app/Http/Controllers/PizzaController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Pizza;
+
+
+class PizzaController extends Controller
+{
+   public function index(){
+
+    $pizzas = pizza::all();
+    return view('pizzas',['pizzas'=> $pizzas,]);
+   } 
+    
+   public function show($id){
+    return view('details',['id'=>$id]);
+   }
+}
